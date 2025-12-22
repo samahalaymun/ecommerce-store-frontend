ed032f7ea56b20a7b681b7f3620b1b9a6da2ac91
samahalaymun <samah_abu_laymun@hotmail.com>
2025-12-22 10:01:38 +0200

    #E-5: test if shdcn library work correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Button } from '@/components/ui/button';
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1 className='bg-red-300 text-lg'>Vite + React</h1>
+      <div className="p-5">
+        <h1 className="bg-red-300 text-lg">Vite + React</h1>
+        <Button variant="secondary" size="sm">
+          check out
+        </Button>
       </div>
     </>
   );
